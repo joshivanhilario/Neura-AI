6346412e4c9947e6153a8dd7dfb5fe3812fbbb13
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-04-21 20:10:02 +0530

    Update the Gemini API Route

--- src/app/api/gemini-ai/route.js
+++ src/app/api/gemini-ai/route.js
@@ -9,16 +9,8 @@ export async function POST(req) {
     const body = await req.json();
     const { messages, selectedModel } = body;
 
-    if (!messages || !Array.isArray(messages)) {
-      return new Response(
-        JSON.stringify({ error: "Invalid messages format. Must be an array." }),
-        { status: 400, headers: { "Content-Type": "application/json" } }
-      );
-    }
-
-    const model = selectedModel ? google(selectedModel) : google();
-    const result = await streamText({
-      model,
+    const result = streamText({
+      model: google(selectedModel || ""),
       system: "You are a helpful assistant",
       messages,
       maxRetries: 3,
@@ -27,16 +19,54 @@ export async function POST(req) {
       }),
     });
 
-    return new Response(result, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    return result.toDataStreamResponse();
   } catch (error) {
     console.error("Error processing the request:", error);
-    return new Response(
-      JSON.stringify({ error: "Something went wrong while processing your request." }),
-      { status: 500, headers: { "Content-Type": "application/json" } }
-    );
+    return new Response(JSON.stringify({ error: "Something went wrong!" }), { status: 500 });
   }
 }
+
+
+// Todo: Passes all Headers and HTTP Code
+// import { google } from "@ai-sdk/google";
+// import { smoothStream, streamText } from "ai";
+
+// export const dynamic = "force-dynamic";
+// export const maxDuration = 35;
+
+// export async function POST(req) {
+//   try {
+//     const body = await req.json();
+//     const { messages, selectedModel } = body;
+
+//     if (!messages || !Array.isArray(messages)) {
+//       return new Response(
+//         JSON.stringify({ error: "Invalid messages format. Must be an array." }),
+//         { status: 400, headers: { "Content-Type": "application/json" } }
+//       );
+//     }
+
+//     const model = selectedModel ? google(selectedModel) : google();
+//     const result = await streamText({
+//       model,
+//       system: "You are a helpful assistant",
+//       messages,
+//       maxRetries: 3,
+//       experimental_transform: smoothStream({
+//         chunking: "word",
+//       }),
+//     });
+
+//     return new Response(result, {
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     });
+//   } catch (error) {
+//     console.error("Error processing the request:", error);
+//     return new Response(
+//       JSON.stringify({ error: "Something went wrong while processing your request." }),
+//       { status: 500, headers: { "Content-Type": "application/json" } }
+//     );
+//   }
+// }

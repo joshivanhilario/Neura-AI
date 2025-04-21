f5ff52e47d3ce85d5781624b0ec4769571782b69
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-04-21 11:30:26 +0530

    Update the nextAPI

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async rewrites() {
+      return [
+        {
+          source: '/api/chat',
+          destination: '/api/groq-ai',
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
--- src/app/api/groq-ai/route.js
+++ src/app/api/groq-ai/route.js
@@ -7,8 +7,7 @@ export const maxDuration = 35;
 export async function POST(req) {
   try {
     const body = await req.json();
-    const messages = body.messages;
-    const selectedModel = body.selectedModel;
+    const { messages, selectedModel } = body;
 
     const result = streamText({
       model: groq(selectedModel || ""),
@@ -22,7 +21,38 @@ export async function POST(req) {
 
     return result.toDataStreamResponse();
   } catch (error) {
-    console.error(error);
-    throw error;
+    console.error("Error processing the request:", error);
+    return new Response(JSON.stringify({ error: "Something went wrong!" }), { status: 500 });
   }
 }
+
+
+
+// import { groq } from "@ai-sdk/groq";
+// import { smoothStream, streamText } from "ai";
+
+// export const dynamic = "force-dynamic";
+// export const maxDuration = 35;
+
+// export async function POST(req) {
+//   try {
+//     const body = await req.json();
+//     const messages = body.messages;
+//     const selectedModel = body.selectedModel;
+
+//     const result = streamText({
+//       model: groq(selectedModel || ""),
+//       system: "You are a helpful assistant",
+//       messages,
+//       maxRetries: 3,
+//       experimental_transform: smoothStream({
+//         chunking: "word",
+//       }),
+//     });
+
+//     return result.toDataStreamResponse();
+//   } catch (error) {
+//     console.error(error);
+//     throw error;
+//   }
+// }
